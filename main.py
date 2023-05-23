43dff58c48052d42b96ed9d9123de02624766081
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-05-23 20:00:32 +0330

    create DiscountError class

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+class DiscountError(Exception):
+    pass
+
+
 def apply_discount(price, discount):
     final_price = int(price * (1 - discount))
     if not 0 <= final_price < price:
-        raise ValueError("Invalid Discount")
+        raise DiscountError("Invalid price")
 
     return final_price
