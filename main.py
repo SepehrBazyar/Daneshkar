e3017c41495d9733b70db151bf2db570648a2306
Sepehr <bazyar.sepehr@gmail.com>
2023-05-23 20:29:34 +0330

    feat: write integer_discount decorator

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ class DiscountError(Exception):
     pass
 
 
+def integer_discount(function):
+
+    def wrapper(price: int, discount: int) -> int:
+        return function(price, discount / 60)
+
+    return wrapper
+
+
+@integer_discount
 def apply_discount(price: int, discount: float = 0.0) -> int:
     """Calculates the Final Price After Discount"""
     final_price = int(price * (1 - discount))
@@ -9,3 +18,6 @@ def apply_discount(price: int, discount: float = 0.0) -> int:
         raise DiscountError("Invalid price")
 
     return final_price
+
+
+print(apply_discount(1000, 30))
