79b364ef01fb120790d8154711b359b69c2cb330
Ramin <eslamiramin85@gmail.com>
2023-05-23 19:50:29 +0330

    create user.py

--- decorator.py
+++ decorator.py
@@ -1,3 +1,6 @@
 def apply_discount(price, discount):
     final_price = int(price * (1 - discount))
-    if not 0 <= final_price <
+    if not 0 <= final_price < final_price < price:
+        raise ValueError('Invalid Discount')
+
+    return final_price
