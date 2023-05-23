6776abbaa8b93e844a2a8162281b70c75a2f3e36
Sepehr <bazyar.sepehr@gmail.com>
2023-05-23 20:03:59 +0330

    style: write type annotations and docstring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def apply_discount(price, discount):
+def apply_discount(price: int, discount: float = 0.0) -> int:
+    """Calculates the Final Price After Discount"""
     final_price = int(price * (1 - discount))
     if not 0 <= final_price < price:
         raise ValueError("Invalid Discount")
